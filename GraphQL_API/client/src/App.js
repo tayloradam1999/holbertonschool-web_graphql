c620eef9daf8092175c5c26e0aa52a4faca2b927
Adam Taylor <tayloradam1999@gmail.com>
2022-01-29 15:19:15 -0600

    Task 9 - setup ApolloClient, pass ApolloClient to ApolloProvider in App() return

--- GraphQL_API/client/src/App.js
+++ GraphQL_API/client/src/App.js
@@ -1,29 +1,29 @@
-/*import ApolloClient from 'apollo-boost';
-/*
-...
-*/
-// components
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import TaskList from './components/TaskList';
 import AddTask from './components/AddTask';
 import AddProject from './components/AddProject';
-// apollo client setup
-/*
-...
-*/
+
+// Apollo client setup
+const client = new ApolloClient({
+	  uri: 'http://localhost:4000/graphql',
+});
+
+// App component, returns ApolloProvider with client before first div tag
 function App() {
-  return ( <
-    div id = "main" >
-    <
-    div id = "bg" > < /div> <
-    h1 > Holberton school tasks < /h1> <
-    TaskList / >
-    <
-    AddProject / >
-    <
-    AddTask / >
-    <
-    /div>
-  );
+  return (
+	  // ApolloProvider is a component that provides the client all of the components in the app
+	<ApolloProvider client={client}>
+		<div id="main">
+			<div id="bg">
+			</div>
+			<h1>Holberton school tasks</h1>
+			<TaskList />
+			<AddProject />
+			<AddTask />
+		</div>
+	</ApolloProvider>
+	);
 }
 
 export default App;
